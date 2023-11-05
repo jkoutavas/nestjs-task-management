abcdc8db741878e8e4fda2a9c1ff673e01c9a65f
jay koutavas <jay@heynow.com>
2023-11-05 09:49:32 -0500

    implemented POST /auth/signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
 import { UserRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -8,5 +10,16 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
