80b10ca1fb8d808deb004f21f6f4d5bf36e01b88
jay koutavas <jay@heynow.com>
2023-11-04 22:05:40 -0400

    POST /auth/signup is working

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+    await this.save(user);
   }
 }
